dbe79dff4f526fb803c26768d425170b1b2f5aff
parvezronr <syedparvezronr.com>
2016-11-30 18:12:42 +0530

    my commit 1

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,3 +1,4 @@
 class Agency < ActiveRecord::Base
 	has_many :packages
+	has_one :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127124847) do
+ActiveRecord::Schema.define(version: 20161129133859) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161127124847) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "packages", force: :cascade do |t|
